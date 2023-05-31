bb8cdd13c783eb21d9a79ea9755f8d7d8e673688
victor <31532226+victorren7@users.noreply.github.com>
2023-05-31 08:57:29 -0700

    added resonsive screensizes

---
+++ src/utils/screensize.js
@@ -0,0 +1,9 @@
+export const screenSize = {
+  desktop: `(min-width: 992px)`,
+  tablet: `(min-width: 767px)`,
+  phone: `(min-width: 575px)`,
+  smallPhone: `(min-width: 319px)`,
+  underDesktop:`(max-width: 991px)`,
+  underSmallPhone:`(max-width: 320px)`,
+
+} 
