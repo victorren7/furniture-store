35204e091f3dcae36999b1859a5274158637a773
victor <31532226+victorren7@users.noreply.github.com>
2023-06-06 16:00:48 -0700

    got started on the product detail page

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
+import styled from 'styled-components';
 
 import { database } from '../../utils/database';
 
@@ -24,8 +25,58 @@ console.log('product', product)
   
   
   return (
-    <div>Product</div>
+    <Container>
+      <ProductRow>
+        <Image alt={product.name} src={product.image} />
+        <Column>
+          <ProductName>{product.name}</ProductName>
+          <Price>${product.price.toLocaleString()}.00</Price>
+          <hr />
+        </Column>
+      </ProductRow>
+      <Carousel>
+
+      </Carousel>
+    </Container>
   )
 }
 
 export default Product
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const ProductRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 15rem;
+  width: 90%;
+  margin: 10rem;
+ border: 1px solid black;
+`
+const Carousel = styled.div`
+  width: 15rem;
+  height: 10rem;
+  border: 1px solid red;
+`
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Image = styled.img`
+  width: 35rem;
+`
+
+const ProductName = styled.span`
+  font-size: 36px;
+`
+
+const Price = styled.span`
+  font-size: 20px;
+`
+
