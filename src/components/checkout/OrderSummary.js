cdbd55c4e724268cfd87055f2d3b34fd9452f67d
victor <31532226+victorren7@users.noreply.github.com>
2024-09-23 14:17:16 -0700

    working on order summary

--- src/components/checkout/OrderSummary.js
+++ src/components/checkout/OrderSummary.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
 import styled from 'styled-components';
 
 function OrderSummary() {
+
+  const { inShoppingCart } = useSelector((store) => store.cart) 
+
+  const itemsMap = () => {
+    return inShoppingCart.map((item, i) => {
+      console.log('item', item)
+      return (
+        <CartRow>
+          <ItemInfo>
+            <Thumbnail alt={item.name} src={item.image} />
+            <CartItemSpan>{item.name}</CartItemSpan>
+          </ItemInfo>
+        </CartRow>
+      )
+    })
+  }
+
   return (
     <Wrapper>
-      OrderSummary
+      <Title>Your Cart</Title>
+      <CartRow>
+        <div>{itemsMap()}</div>
+      </CartRow>
     </Wrapper>
   )
 }
@@ -19,4 +41,31 @@ const Wrapper = styled.div`
   padding: 1rem;
   background-color: moccasin;
   border-radius: 2rem;
+`
+
+const Title = styled.span`
+  font-size: 26px;
+  margin-bottom: 3.5rem;
+`
+
+const CartRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const ItemInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const Thumbnail = styled.img`
+  width: 4rem;
+  height: 4rem;
+  margin-right: 1rem;
+`
+
+const CartItemSpan = styled.span`
+  font-size: 18px;
+  align-self: center;
 `
